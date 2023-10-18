58486c0ad56e78c036c43a5cdccd4be768c172d4
trandat1114 <cctalk1114@gmail.com>
2023-10-18 09:47:28 +0700

    🔥🔥🔥 REAL FINAL 🎫🌞🌞

--- search_app/src/pages/home.tsx
+++ search_app/src/pages/home.tsx
@@ -9,16 +9,16 @@ export const HomePage = () => {
                     <div className='container mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 relative rounded-md border p-5'>
                         <div className="grid grid-cols-1 justify-center items-center gap-5">
                             <div className="flex justify-between w-full">
-                                <Link to={"/organization"}><p className="text-xl font-mono">Organizations</p></Link>
+                                <Link to={"/organization"}><p className="text-xl font-mono underline text-primary">Organizations</p></Link>
                                 ...list Organization
                             </div>
                             <hr />
                             <div className="flex justify-between w-full">
-                                <Link to={"/ticket"}><p className="text-xl font-mono">Tickets</p></Link> ...list Ticket
+                                <Link to={"/ticket"}><p className="text-xl font-mono underline text-primary">Tickets</p></Link> ...list Ticket
                             </div>
                             <hr />
                             <div className="flex justify-between w-full">
-                                <Link to={"/user"}><p className="text-xl font-mono">Users</p></Link> ...list User
+                                <Link to={"/user"}><p className="text-xl font-mono underline text-primary">Users</p></Link> ...list User
                             </div>
                         </div>
                     </div>
