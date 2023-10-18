9eec2231467e09613934a01891394f160783ff74
trandat1114 <cctalk1114@gmail.com>
2023-10-18 09:30:28 +0700

    UPDATE HOME

--- search_app/src/pages/home.tsx
+++ search_app/src/pages/home.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom"
 
 
 export const HomePage = () => {
     return (
         <>
-
+            <Link to={"/organization"}>Organizations</Link>
+            <Link to={"/ticket"}>Tickets</Link>
+            <Link to={"/user"}>Users</Link>
         </>
     )
 }
