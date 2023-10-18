cdbbbba1c8503844b37b88f69a40538c146f4caa
trandat1114 <cctalk1114@gmail.com>
2023-10-18 09:33:53 +0700

    UPDATE 🤔

--- search_app/src/pages/home.tsx
+++ search_app/src/pages/home.tsx
@@ -4,8 +4,11 @@ import { Link } from "react-router-dom"
 export const HomePage = () => {
     return (
         <>
+
             <Link to={"/organization"}>Organizations</Link>
+            <hr />
             <Link to={"/ticket"}>Tickets</Link>
+            <hr />
             <Link to={"/user"}>Users</Link>
         </>
     )
